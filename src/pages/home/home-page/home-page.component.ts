5f9b42ce6b867fc6bd816459ae2d4d3977740174
quye111@163.com <quye111@163.com>
2017-07-27 19:39:09 +0800

    MODIFY sortByAsccending & sortByDesccending & sortByRadom & README

--- src/pages/home/home-page/home-page.component.ts
+++ src/pages/home/home-page/home-page.component.ts
@@ -8,56 +8,72 @@ import { Meta, Title } from '@angular/platform-browser';
 })
 export class HomePageComponent implements OnInit {
   users:Array<any> = [
-    {"index":19,
-    "name":"刘雨飏",
-      "sex":"Male",
-      "name_en":"Ryane",
-      "github":"ryanemax",
-      "exam1":66,
-      "exam2":66,
-      "exam3":66},
-      {"index":7,
-      "name":"小明",
-      "sex":"Male",
-      "name_en":"Ryane",
-      "github":"ryanemax",
-      "exam1":66,
-      "exam2":66,
-      "exam3":66},
-      {"index":14,
-      "name":"Chunk",
-      "sex":"Male",
-      "name_en":"Chunk",
-      "github":"chunk",
-      "exam1":76,
-      "exam2":76,
-      "exam3":76}
+    {"name":"丰田",
+    "name_en":"TOYOTA",
+      "country":"Japan",
+      "sub_name":"corrola",
+      "sale_amount":8970},
+    {"name":"本田",
+      "name_en":"HONDA",
+      "country":"Japan",
+      "sub_name":"civic",
+      "sale_amount":7779},
+    {"name":"福特",
+      "name_en":"FORD",
+      "country":"USA",
+      "sub_name":"focus",
+      "sale_amount":6780},
+    {"name":"大众",
+      "name_en":"DASAUTO",
+      "country":"German",
+      "sub_name":"lavida",
+      "sale_amount":9080}
   ]
 deleteLast(){
   this.users.pop()
 }
 saveNewUser(){
   this.users.push({
-     "index":666,
-      "name":"New User",
-      "sex":"Male",
-      "name_en":"new_user",
+     "name":"日产",
+      "name_en":"NISSAN",
+      "country":"Japan",
+      "sub_name":"Bluebird",
       "github":"new_user",
-      "exam1":76,
-      "exam2":76,
-      "exam3":76
+      "sale_amount":7980
   })
 }
   sortByAsccending(){
     // 参考MDN Array操作的API文档 Array相关方法方法
+    this.users.sort(function (a,b){
+      if (a.sale_amount >= b.sale_amount){
+        return 1;
+      } 
+      if (a.sale_amount < b.sale_amount){}
+        return -1;
+    })
   }
   sortByDesccending(){
     // 参考MDN Array操作的API文档 Array相关方法
     // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
+    this.users.sort(function (a,b){
+      if (a.sale_amount <= b.sale_amount){
+        return 1;
+      } 
+      if (a.sale_amount > b.sale_amount){}
+        return -1;
+    })
   }
   sortByRadom(){
     // 参考MDN Array操作的API文档 Math相关方法
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+    this.users.sort(function(a,b){
+      if (a.sale_amount * Math.random() >= b.sale_amount * Math.random()){
+        return 1;
+      }
+      if (a.sale_amount * Math.random() < b.sale_amount * Math.random()){
+        return -1;
+      }
+    })
   }
   constructor(meta: Meta, title: Title) {
     title.setTitle('My Home Page');
